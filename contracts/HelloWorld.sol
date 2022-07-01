ff750419ba333835e66d69956cf03dea4549abb9
"Janus.sol" <"gcasolo.csisrls@gmail.com">
2022-07-01 18:47:32 +0200

    my first edit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,14 @@
+ pragma solidity ^0.8.0;
+ contract HelloWorld
+
+ {
+
+ uint private stateVariable = 1;
+ function GetHelloWorld() public view returns (uint)
+
+ {
+ return stateVariable;
+ }
+
+ }
+ 
