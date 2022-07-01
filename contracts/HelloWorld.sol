4b4170e087d78f2072a437c1c07edcbd8fec6f88
"Janus.sol" <"gcasolo.csisrls@gmail.com">
2022-07-01 19:32:58 +0200

    Variables

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,14 +1,54 @@
- pragma solidity ^0.8.0;
- contract HelloWorld
+/* 
+* author: Janus.sol
+* github: https//github.com/Janus.sol
+* date: 01/07/2022
+* SPDX-License-Identifier: MIT
+*/
+pragma solidity ^0.8.0;
+contract HelloWorld {
 
- {
+    //dichiarazione
+    //inizializzazione
+    //get set
 
- uint private stateVariable = 1;
- function GetHelloWorld() public view returns (uint)
+    //storage memory
 
- {
- return stateVariable;
- }
+    //variabili -> statically types -> referece types
 
- }
- 
+    //uint256 -> 2**256-1
+    //uint8 16 32 64 128 256
+    uint public stateVariable = 10;
+    int public stateVariableInt = -2;
+    string public varString = "a"; //""
+    bool public varBool = true; //true false
+    address public varAddress;
+    uint[] public varArrayUint;
+    mapping(uint => bool) varMapping;
+    //Structs Enum
+
+    function getHelloWorld() public returns (uint) {
+        stateVariable = 5;
+        return stateVariable;
+    }
+
+    function getStateVariable() public view returns (uint) {
+        return stateVariable;
+    }
+
+
+    function getSmartContractAddress() public view returns (address) {
+        return address(this); //cast
+    }
+
+    
+    function getSmartContractBalance() public view returns (uint) {
+        return address(this).balance; //cast
+    }
+
+    //address balance transfer
+
+    function getA() public view returns(uint) {
+        uint a = uint(stateVariableInt);
+        return a;
+    }
+}
